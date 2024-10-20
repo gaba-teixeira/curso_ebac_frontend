d898e4ebbef39edab79fe23a6f6fc3fb3b869065
gaba-teixeira <gaba.andrade@outlook.com>
2024-10-20 16:30:36 -0300

    Finished js functions

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const form = document.getElementById("formulario");
 let linhas = "";
 const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando"/>';
 const imgReprovado = '<img src="./images/reprovado.png" alt="emoji triste"/>';
+const atividades = [];
+const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  adicionarLinha();
+  atualizarTabela();
+  atualizarMedia();
+});
 
+function adicionarLinha() {
   const inputAtividade = document.getElementById("form-atividade");
   const inputNota = document.getElementById("form-nota");
+  atividades.push(inputAtividade.value);
+  notas.push(parseFloat(inputNota.value));
 
   let linha = "<tr>";
   linha += `<td>${inputAtividade.value}</td>`;
@@ -16,10 +28,27 @@ form.addEventListener("submit", function (e) {
   linha += "</tr>";
 
   linhas += linha;
-
-  const corpoTabela = document.querySelector("tbody");
-  corpoTabela.innerHTML = linhas;
-
   inputAtividade.value = "";
   inputNota.value = "";
-});
+}
+
+function atualizarTabela() {
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+}
+
+function atualizarMedia() {
+  let mediaFinal = calcularMedia();
+  document.getElementById("media-final").innerHTML = mediaFinal;
+  document.getElementById("media-resultado").innerHTML =
+    mediaFinal >= 7 ? spanAprovado : spanReprovado;
+}
+
+function calcularMedia() {
+  somaNotas = 0;
+
+  for (i = 0; i < notas.length; i++) {
+    somaNotas += notas[i];
+  }
+  return somaNotas / notas.length;
+}
